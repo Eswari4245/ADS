32305232692b0af22e3354b2078978cb081017e7
Eswari4245 <adigarlaeswari@gmail.com>
2025-12-18 19:49:33 +0530

    Add files via upload

---
+++ palindrome or not.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int num, rev = 0, temp;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+    while (temp != 0) {
+        rev = rev * 10 + temp % 10;
+        temp /= 10;
+    }
+
+    if (num == rev)
+        printf("Palindrome number");
+    else
+        printf("Not a palindrome");
+
+    return 0;
+}
+
